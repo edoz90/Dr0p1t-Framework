24accbf37533bee441bbb705409f71a3bca86d5b
Edoardo Rosa <edoardo.rosa90@gmail.com>
2017-12-17 21:58:53 +0100

    fix path

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ wget https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi
 wine msiexec /i python-2.7.14.msi /L*v log.txt
 wget https://bootstrap.pypa.io/get-pip.py
 wine ${HOME}/.wine/drive_c/Python27/python.exe get-pip.py
-wine ${HOME}.wine/drive_c/Python27/python.exe -m pip install pyinstaller
+wine ${HOME}/.wine/drive_c/Python27/python.exe -m pip install pyinstaller
